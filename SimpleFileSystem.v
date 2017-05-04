aa831ee054042171b679dca511cbbfe578ec71a5
Yao Li <hnkfliyao@gmail.com>
2017-05-04 14:16:29 -0400

    Implement SimpleFS with new specs.

--- SimpleFileSystem.v
+++ SimpleFileSystem.v
@@ -34,6 +34,8 @@ Module SimpleFS : FileSystem.
     if strcmp p file_name then
       Some (true :: nil)
     else None.
+
+  Hint Unfold fsContents.
     
   Record file_system' : Type :=
     mkFS { is_open : bool;
@@ -65,29 +67,31 @@ Module SimpleFS : FileSystem.
                stat : abstract_file_stat
              }.
 
-  Record abstract_file_system : Type :=
-    abs { contents : path -> option (list bool);
-          streams : list file;
-          file_info : file -> option abstract_file_metainfo;
-          file_no : file -> option file_handle
-        }.
-
-  Definition FS ( A : Type ) := file_system -> (A * file_system).
-
   Definition abs_fstat (f : file_stat) : abstract_file_stat :=
     abs_stat (is_reg f) (is_dir f) (is_chr f) (is_blk f)
              (is_fifo f) (is_lnk f) (is_sock f).
 
   Definition init_abs_fstat := abs_fstat init_file_stat.
+  
+  Definition contents (fs : file_system) := fsContents.
+  Definition streams (fs : file_system) :=
+    if is_open fs then tt::nil else nil.
+  Definition file_info (fs : file_system) (_ : file) :=
+    if is_open fs then
+      Some (abs_file file_name (if is_read fs then 1 else 0)
+                     init_abs_fstat)
+    else None.
+  Definition file_no (fs : file_system) (_ : file) :=
+    if (has_fd fs) then Some 1 else None.
+  
+  Definition FS ( A : Type ) := file_system -> (A * file_system).
 
-  Definition abs_fs (fs: file_system) : abstract_file_system :=
-    abs fsContents (if is_open fs then tt::nil else nil)
-        (fun _ => if is_open fs then
-                 Some (abs_file file_name (if is_read fs then 1 else 0)
-                                init_abs_fstat)
-               else None)
-        (fun _ => if (has_fd fs) then Some 1 else None).
+  Hint Unfold contents.
+  Hint Unfold streams.
+  Hint Unfold file_info.
+  Hint Unfold file_no.
 
+  (*
   Theorem abstract_file_system_spec : forall fs f,
       forall afs, afs = abs_fs fs ->
              (In f (streams afs) <-> file_info afs f <> None) /\
@@ -102,6 +106,7 @@ Module SimpleFS : FileSystem.
       destruct (is_open fs); subst; inversion H;
         simpl; intro; try solve by inversion.
   Qed.
+   *)
   
   Definition fopen (p: path) (m : file_access_mode) : FS (option file) :=
     fun fs => match m with
@@ -114,29 +119,27 @@ Module SimpleFS : FileSystem.
 
   Theorem fopen_spec : forall p m f fs fs',
       fopen p m fs = (f, fs') ->
-      forall afs, afs = abs_fs fs ->
-      forall afs', afs' = abs_fs fs' ->
-      (m = rb -> contents afs p = None -> f = None) /\
+      (m = rb -> contents fs p = None -> f = None) /\
       (m = wb \/ m = ab ->
        forall f', f = Some f' ->
-             contents afs' p <> None) /\
+             contents fs' p <> None) /\
       (forall f', f = Some f' ->
-             In f' (streams afs')) /\
-      (forall f', file_no afs f' = file_no afs' f') /\
+             In f' (streams fs')) /\
+      (forall f', file_no fs f' = file_no fs' f') /\
       (forall f', f <> Some f' ->
-             file_info afs f' = file_info afs' f' /\
-             In f' (streams afs) = In f' (streams afs')) /\
+             file_info fs f' = file_info fs' f' /\
+             In f' (streams fs) = In f' (streams fs')) /\
       (forall p', p <> p' \/ m = rb ->
-             contents afs p' = contents afs' p').
+             contents fs p' = contents fs' p').
   Proof.
     intros. repeat split.
     - intros; subst.
-      inversion H3. unfold fsContents in H1.
+      unfold contents in H1. unfold fsContents in H1.
       unfold fopen in H.
       destruct (strcmp p0 file_name).
       + try solve by inversion.
       + inversion H; auto.
-    - intros. destruct H2; subst; simpl in *; try solve by inversion.
+    - intros. destruct H0; subst; simpl in *; try solve by inversion.
     - intros. subst. destruct m; simpl in H; try solve by inversion.
       destruct (strcmp p0 file_name); try solve by inversion.
       inversion H. simpl. auto.
@@ -146,13 +149,11 @@ Module SimpleFS : FileSystem.
     - subst.
       destruct m; simpl in H; destruct (strcmp p0 file_name);
         inversion H; subst; auto.
-      + simpl. destruct (is_open fs); auto.
-        destruct f'. contradiction.
+      + simpl. destruct (is_open fs); destruct f'; contradiction.
     - destruct f.
       + destruct f; destruct f'. contradiction.
       + destruct m; simpl in H; destruct (strcmp p0 file_name);
           subst; inversion H; auto.
-    - intros; subst. simpl; auto.
   Qed.
 
   Definition fileno (f : file) : FS (option file_handle) :=
@@ -162,32 +163,28 @@ Module SimpleFS : FileSystem.
 
   Theorem fileno_spec : forall f fs fs' fd,
       fileno f fs = (fd, fs') ->
-      forall afs, afs = abs_fs fs ->
-      forall afs', afs' = abs_fs fs' ->       
-      (In f (streams afs) ->
-       file_no afs' f = fd /\
-       (file_no afs f <> None -> file_no afs f = fd)) /\
-      (~ In f (streams afs) -> fd = None) /\
+      (In f (streams fs) ->
+       file_no fs' f = fd /\
+       (file_no fs f <> None -> file_no fs f = fd)) /\
+      (~ In f (streams fs) -> fd = None) /\
       (forall f', f' <> f ->
-             file_no afs' f <> file_no afs' f' /\
-             file_no afs f' = file_no afs' f') /\
-      (forall f', file_info afs f' = file_info afs' f') /\
-      (forall f', In f' (streams afs) <-> In f' (streams afs')) /\
-      (forall p', contents afs p' = contents afs' p').
+             file_no fs' f <> file_no fs' f' /\
+             file_no fs f' = file_no fs' f') /\
+      (forall f', file_info fs f' = file_info fs' f') /\
+      (forall f', In f' (streams fs) <-> In f' (streams fs')) /\
+      (forall p', contents fs p' = contents fs' p').
   Proof.
     intros. unfold fileno in H.
     destruct (is_open fs) eqn:Hopen.
-    - repeat split; subst; inversion H; simpl; auto;
-        try (destruct f; destruct f'; contradiction);
-          try (rewrite Hopen); auto;
-            try solve [destruct f; simpl; intro Hc; exfalso; apply Hc; auto].
-      + destruct (has_fd fs); auto. intro. contradiction.
-    - repeat split; subst; inversion H; simpl; auto;
-        try (destruct f; destruct f'; contradiction);
-        destruct (has_fd fs) eqn:Hfd; inversion H; subst;
-          try (rewrite Hopen); try (rewrite Hfd); auto;
-            try solve [unfold abs_fs in H2; rewrite Hopen in H2;
-                       simpl in H2; exfalso; auto].
+    - repeat split; subst; inversion H; subst; simpl; auto.
+      all: try (destruct f; destruct f'; contradiction).
+      all: autounfold; try rewrite Hopen; simpl; auto.
+      + destruct (has_fd fs); auto; contradiction.
+      + destruct f. intros. destruct H0. simpl. auto.
+    - repeat split; subst; inversion H; subst; simpl; auto.
+      all: try (destruct f; destruct f'; contradiction).
+      all: autounfold in *; rewrite Hopen in *;
+        destruct f; simpl in *; try solve by inversion.
   Qed.
 
   Definition fseek (f : file) (off : nat) (s : seek_set) : FS bool :=
@@ -203,38 +200,34 @@ Module SimpleFS : FileSystem.
   
   Theorem fseek_spec : forall f off origin fs fs' b,
       fseek f off origin fs = (b, fs') ->
-      forall afs, afs = abs_fs fs ->
-      forall afs', afs' = abs_fs fs' ->       
-      (In f (streams afs) <-> b = true) /\
-      (forall f', file_no afs f' = file_no afs' f') /\
-      (forall f', f <> f' -> file_info afs f' = file_info afs' f') /\
-      (forall fi,  file_info afs  f = Some fi  ->
-       forall fi', file_info afs' f = Some fi' ->
-       forall s, contents afs (p fi) = Some s ->
+      (In f (streams fs) <-> b = true) /\
+      (forall f', file_no fs f' = file_no fs' f') /\
+      (forall f', f <> f' -> file_info fs f' = file_info fs' f') /\
+      (forall fi,  file_info fs  f = Some fi  ->
+       forall fi', file_info fs' f = Some fi' ->
+       forall s, contents fs (p fi) = Some s ->
             offset fi' = min (length s) (expected_offset origin (offset fi) (length s) off)) /\
-      (forall f', In f' (streams afs) <-> In f' (streams afs')) /\
-      (forall p', contents afs p' = contents afs' p').
+      (forall f', In f' (streams fs) <-> In f' (streams fs')) /\
+      (forall p', contents fs p' = contents fs' p').
   Proof.
     intros.
     destruct off; unfold fseek in H;
       destruct (is_open fs) eqn:Hopen; simpl in H; inversion H; subst;
-        destruct origin eqn:Horigin;
-        repeat split; try intros; inversion H; subst; auto;
-          try (destruct f; destruct f'; contradiction);
-          try solve [simpl; rewrite Hopen; destruct f; simpl; auto];
-          try solve [simpl in *; rewrite Hopen in *;
-                     inversion H0; inversion H1; simpl;
-                     try (destruct (is_read fs'));
-                     try (simpl; simpl in H2;
-                          unfold fsContents in H2; subst;
-                          simpl in H2; inversion H2; simpl);
-                     try rewrite Nat.min_0_r; reflexivity];
-          try (simpl; destruct f'); auto.
-    - simpl. simpl in H1; inversion H1. simpl.
-      simpl in H2. unfold fsContents in H2.
-      simpl in H0. rewrite Hopen in H0. inversion H0.
-      subst. simpl in H2. inversion H2. simpl.
-      rewrite <- plus_n_Sm. auto.
+        destruct origin eqn:Horigin; repeat split.
+    all: try intros; inversion H; subst; auto.
+    all: autounfold in *; rewrite Hopen in *.
+    all: try (destruct f).
+    all: try (destruct f').
+    all: simpl in *; auto; try contradiction.
+    all: try
+           (match goal with
+              [ H : fsContents ?pi = Some ?s |- _ ] => 
+              autounfold in H; destruct (strcmp pi file_name); inversion H end).
+    all: try inversion H0; simpl; auto.
+    all: try inversion H1; simpl; auto.
+    all: try inversion H2; simpl; auto.
+    - destruct (is_read fs'); simpl; auto.
+    - destruct (is_read fs); simpl; auto.
   Qed.
 
   Definition fread (f : file) (size : nat) (count : nat) : FS (list (list bool) * nat) :=
@@ -252,94 +245,50 @@ Module SimpleFS : FileSystem.
 
   Theorem fread_spec : forall f size count fs fs' buf r,
       fread f size count fs = ((buf, r), fs') ->
-      forall afs, afs = abs_fs fs ->
-      forall afs', afs' = abs_fs fs' ->
       (r <= count) /\
       (length (filter (fun b => beq_nat (length b) size) buf) = r) /\
-      (~ In f (streams afs) -> buf = nil /\ r = 0) /\
-      (forall f', file_no afs f' = file_no afs' f') /\
+      (~ In f (streams fs) -> buf = nil /\ r = 0) /\
+      (forall f', file_no fs f' = file_no fs' f') /\
       (forall f', f <> f' ->
-             file_info afs f' = file_info afs' f') /\
+             file_info fs f' = file_info fs' f') /\
       (forall l, l = fold_left (fun x y => x + length y) buf 0 ->
-       forall fi,  file_info afs  f = Some fi  ->
-       forall fi', file_info afs' f = Some fi' ->
-       forall s, contents afs (p fi) = Some s ->
+       forall fi,  file_info fs  f = Some fi  ->
+       forall fi', file_info fs' f = Some fi' ->
+       forall s, contents fs (p fi) = Some s ->
             l <= (length s - offset fi) /\ offset fi' = offset fi + l) /\
-      (forall f', In f' (streams afs) <-> In f' (streams afs')) /\
-      (forall p', contents afs p' = contents afs' p').
+      (forall f', In f' (streams fs) <-> In f' (streams fs')) /\
+      (forall p', contents fs p' = contents fs' p').
   Proof.
     intros. repeat split.
-    - unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread;
-        try solve [inversion H; omega].
-      destruct (beq_nat size 1) eqn:Hsize';
-        inversion H; try apply beq_nat_false in Hcount; omega.
-    - unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread; inversion H; simpl; auto.
-      destruct size; simpl; auto.
-      destruct size; simpl; auto.
-    - unfold abs_fs in H0; subst. simpl in H2.
-      destruct f; destruct (is_open fs) eqn:Hopen; simpl in H2.
-      + exfalso. apply H2. auto.
-      + unfold fread in H. rewrite Hopen in H.
-        destruct (beq_nat size 0); destruct (beq_nat count 0);
-          inversion H; auto.
-    - unfold abs_fs in H0; subst. simpl in H2.
-      destruct f; destruct (is_open fs) eqn:Hopen; simpl in H2.
-      + exfalso. apply H2. auto.
-      + unfold fread in H. rewrite Hopen in H.
-        destruct (beq_nat size 0); destruct (beq_nat count 0);
-          inversion H; auto.
-    - intros. unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread; inversion H; subst; auto.
-    - intros. destruct f; destruct f'; contradiction.
-    - rewrite H0 in H5. simpl in H5. unfold fsContents in H5.
-      rewrite H0 in H3. simpl in H3.
-      destruct (is_open fs); try solve by inversion.
-      inversion H3. subst. simpl in H5. inversion H5. simpl.
-      unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread; inversion H; simpl; omega.
-    - subst. simpl in H5. simpl in H3. simpl in H4.
-      destruct (is_open fs); try solve by inversion.
-      inversion H3. subst. unfold fsContents in H5. simpl in H5.
-      inversion H5. simpl.
-      unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread; inversion H; subst;
-          try (rewrite Hopen in H4);
-          try (inversion H4; simpl; try rewrite Hread; auto).
-    - unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread; inversion H; subst; auto.
-      unfold abs_fs. simpl. destruct f'. intro. auto.
-    - unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread; inversion H; subst; auto.
-      unfold abs_fs. simpl. destruct f'. intro. rewrite Hopen. auto.
-    - intros.
-      unfold fread in H.
-      destruct (beq_nat size 0) eqn:Hsize;
-        destruct (beq_nat count 0) eqn:Hcount;
-        destruct (is_open fs) eqn:Hopen;
-        destruct (is_read fs) eqn:Hread; inversion H; subst; auto.
+    all: try match goal with
+               [ H : fread _ _ _ _ = _ |- _ ] =>
+               unfold fread in H;
+                 destruct (beq_nat size 0) eqn:Hsize;
+                 destruct (beq_nat count 0) eqn:Hcount;
+                 destruct (is_open fs) eqn:Hopen;
+                 destruct (is_read fs) eqn:Hread; 
+                 try solve [inversion H; omega];
+                 try solve [inversion H; simpl; auto];
+                 try solve [destruct (beq_nat size 1) eqn:Hsize';
+                            inversion H; try apply beq_nat_false in Hcount; omega]
+             end.
+    all: autounfold in *.
+    all: try (rewrite Hopen in *).
+    all: try (destruct f'; intros).
+    all: try solve [destruct f; destruct H0; simpl; auto].
+    all: try
+           (match goal with
+              [ H : fsContents ?pi = Some ?s |- _ ] => 
+              autounfold in H; destruct (strcmp pi file_name); inversion H end).
+    all: try (inversion H1; simpl; rewrite Hread;
+              inversion H; subst; simpl; auto).
+    all: try simpl in H2.
+    all: try (rewrite Hopen in H2; rewrite Hread in H2).
+    all: try (inversion H2; simpl; auto).
+    all: try (inversion H).
+    all: simpl; auto.
+    - destruct size eqn:Hsize'; try solve by inversion.
+      simpl in H. destruct n; simpl in H; inversion H; simpl; auto.
   Qed.
 
   Definition fclose (f : file) : FS bool :=
@@ -349,23 +298,22 @@ Module SimpleFS : FileSystem.
 
   Theorem fclose_spec : forall f fs fs' b,
       fclose f fs = (b, fs') ->
-      forall afs, afs = abs_fs fs ->
-      forall afs', afs' = abs_fs fs' ->
-      (~ In f (streams afs) -> b = false) /\
-      ~ In f (streams afs') /\
-      (forall f', file_no afs f' = file_no afs' f') /\
+      (~ In f (streams fs) -> b = false) /\
+      ~ In f (streams fs') /\
+      (forall f', file_no fs f' = file_no fs' f') /\
       (forall f', f <> f' ->
-             file_info afs f' = file_info afs' f' /\
-             In f' (streams afs) = In f' (streams afs')) /\
-      (forall p', contents afs p' = contents afs' p').
+             file_info fs f' = file_info fs' f' /\
+             In f' (streams fs) = In f' (streams fs')) /\
+      (forall p', contents fs p' = contents fs' p').
   Proof.
     intros. unfold fclose in H.
-    destruct (is_open fs) eqn:Hopen; inversion H; repeat split;
-      try (unfold abs_fs in H0; unfold abs_fs in H1; subst; simpl);
-      try rewrite Hopen; auto.
-    - destruct f. intro. exfalso. apply H0. simpl. auto.
-    - destruct f; destruct f'. exfalso. apply H2. reflexivity.
-    - destruct f; destruct f'. exfalso. apply H2. reflexivity.
+    destruct (is_open fs) eqn:Hopen; inversion H; repeat split.
+    all: autounfold.
+    all: destruct f. 
+    all: try rewrite Hopen; simpl; auto.
+    all: try (destruct f'; contradiction).
+    - intros. destruct H0. auto.
+    - subst. rewrite Hopen. auto.
   Qed.
 
   Definition fstat (fd : file_handle) (fs : file_system) : option file_stat :=
@@ -374,18 +322,19 @@ Module SimpleFS : FileSystem.
     else None.
 
   Theorem fstat_spec : forall f fd fs st,
-      forall afs, abs_fs fs = afs ->
-      file_no afs f = Some fd ->
+      file_no fs f = Some fd ->
       fstat fd fs = st ->
-      forall fi, file_info afs f = Some fi ->
+      forall fi, file_info fs f = Some fi ->
       forall st', st = Some st' ->
       stat fi = abs_fstat st'.
   Proof.
-    intros.
-    destruct (beq_nat fd 1) eqn:Heq; subst;
-      try (unfold fstat in H3; rewrite Heq in H3; inversion H3); subst.
-    - inversion H2. destruct (is_open fs); inversion H1.
-      simpl. unfold init_abs_fstat. reflexivity.
+    intros. autounfold in *.
+    destruct (beq_nat fd 1) eqn:Heq; subst.
+    all: unfold fstat in H2; rewrite Heq in H2; inversion H2.
+    subst. unfold abs_fstat. simpl in *.
+    destruct (is_open fs); destruct (has_fd fs);
+      try solve by inversion.
+    inversion H1. simpl. auto.
   Qed.
 
   Theorem is_reg_spec : forall st,
