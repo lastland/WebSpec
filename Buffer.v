23ce96b1b1c4f9d6a91ab37375cf2418e7942e44
Yao Li <hnkfliyao@gmail.com>
2017-05-08 19:54:18 -0400

    httpd interface.

---
+++ Buffer.v
@@ -0,0 +1,3 @@
+Require Import String.
+
+Definition buffer ( A : Type ) := string -> ( A * string ).
--- HttpdInterface.v
+++ HttpdInterface.v
@@ -1,40 +1,69 @@
 Require Import List.
 Require Import String.
 
+Require Import Buffer.
 Require Import Resource.
 
-Module Type Httpd.
+Inductive responseMemoryMode : Type :=
+| Persistent : responseMemoryMode
+| MustFree : responseMemoryMode
+| MustCopy : responseMemoryMode.
 
-  Declare Module Res : Resource.
-  
-  Inductive responseMemoryMode : Type :=
-  | Persistent : responseMemoryMode
-  | MustFree : responseMemoryMode
-  | MustCopy : responseMemoryMode.
-
-  Definition contentReaderCallback : Type :=
-    Res.resource -> nat -> string -> nat -> nat.
-
-  Definition contentReaderFreeCallback : Type :=
-    Res.resource -> Res.resource.
-  
+Module Type Httpd (res: Resource).
+  Parameter daemon : Type.
   Parameter response : Type.
   Parameter connection : Type.
-  Parameter createResponseFromBuffer :
-    nat -> string -> responseMemoryMode -> response.
-  Parameter createResponseFromCallback :
-    nat -> nat -> contentReaderCallback -> Res.resource ->
-    contentReaderFreeCallback -> response.
-  Parameter queueResponse :
-    connection -> nat -> response -> option connection.
+  Definition DM ( A : Type ) := daemon -> (A * daemon).
 
-  Parameter response_in_queue : connection -> response -> bool.
-  Definition response_in_queue_option
-             (conn : option connection) (res : response) : bool :=
-    match conn with
-    | Some c => response_in_queue c res
-    | None => true
-    end.
-  Axiom response_in_queue_after_queue_response :
-    forall conn n res, response_in_queue_option (queueResponse conn n res) res = true.
+  Definition RS ( A : Type ) := res.resource -> (A * res.resource).
+  
+  (** Callbacks. Used as function pointers in C program. **)
+  Definition contentReaderCallback : Type :=
+    res.class -> nat -> nat -> buffer (RS bool).
+
+  Definition contentReaderFreeCallback : Type :=
+    res.class -> RS unit.
+
+  (** Httpd interfaces. **)
+  Parameter create_response_from_buffer :
+    nat -> responseMemoryMode -> buffer (DM (option response)).
+  Parameter create_response_from_callback :
+    nat -> nat -> contentReaderCallback -> res.resource ->
+    contentReaderFreeCallback -> DM (option response).
+  Parameter queue_response :
+    connection -> nat -> response -> DM bool.
+
+  (** Abstract states. **)
+  Parameter response_queue : daemon -> list (connection * nat * response).
+  Parameter response_content : response -> option string.
+  Parameter response_crc : response -> option contentReaderCallback.
+  Parameter response_crfc : response -> option contentReaderFreeCallback.
+  Parameter response_crc_cls : response -> option res.resource.
+
+  (** Specifications. **)
+  Axiom create_response_from_buffer_spec :
+    forall size mode buf d rd buf' r d',
+      create_response_from_buffer size mode buf = (rd, buf') ->
+      rd d = (r, d') ->
+      response_queue d = response_queue d' /\
+      buf = buf' /\
+      (forall res, r = Some res ->
+       exists b, response_content res = Some b /\
+       length b <= size /\
+       substring 0 size buf = b).
+
+  Axiom create_response_from_callback_spec :
+    forall size blk_size crc resource crfc d r d',
+      create_response_from_callback size blk_size crc resource crfc d = (r, d') ->
+      response_queue d = response_queue d' /\
+      (forall res, r = Some res ->
+       response_crc res = Some crc /\
+       response_crfc res = Some crfc /\
+       response_crc_cls res = Some resource).
+
+  Axiom queue_response_spec :
+    forall conn status res b d d',
+      queue_response conn status res d = (b, d') ->
+      (forall r, In r (response_queue d) -> In r (response_queue d')) /\
+      In (conn, status, res) (response_queue d').
 End Httpd.
